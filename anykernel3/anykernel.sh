800d69b02e24678332ba29273227763e6ee39d07
amorfati010209-cyber <amorfati010209@gmail.com>
2026-07-01 23:34:00 +0000

    Fix YAML parse error and update device names for CPH2573
    - Replace inline heredoc in Package step with cp from repo file
      (heredoc content at column 0 broke YAML block scalar parsing)
    - Update anykernel3/anykernel.sh with correct device identifiers:
      CPH2573 / OP595DL1 / CPH2573IN (global OnePlus 12)
      rather than OPD2403/waffle (CN model)

--- anykernel3/anykernel.sh
+++ anykernel3/anykernel.sh
@@ -8,11 +8,11 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=OP5959L1
-device.name2=OPD2403
-device.name3=waffle
-device.name4=OP5959EEA
-device.name5=OnePlus12
+device.name1=CPH2573
+device.name2=OP595DL1
+device.name3=CPH2573IN
+device.name4=OP5959L1
+device.name5=OPD2403
 supported.versions=16
 supported.patchlevels=
 '; }
